589b8d73490e788872cfa1aa0a5675fc072bfb93
tae min kim <taemintkim@berkeley.edu>
2016-03-09 20:15:21 -0800

    working on API

--- mobile/src/main/java/com/cs160/joleary/represent/CongressionalActivity.java
+++ mobile/src/main/java/com/cs160/joleary/represent/CongressionalActivity.java
@@ -1,17 +1,35 @@
 package com.cs160.joleary.represent;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.cs160.joleary.represent.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Random;
 
 public class CongressionalActivity extends Activity {
@@ -35,6 +53,9 @@ public class CongressionalActivity extends Activity {
     Button button2;
     Button button3;
 
+    public static String sunlightURL1 = "congress.api.sunlightfoundation.com/";
+    public static String sunlightURL2 = "&apikey=8385878a45504ea29ded263a1d2f3f81";
+
     ArrayList<Representative> reps = new ArrayList<>();
 
     int zip = 0;
@@ -51,6 +72,7 @@ public class CongressionalActivity extends Activity {
         if (zip == -9999) {
             random();
         }
+        fetchRepData(zip);
         updateViews();
         sendToWatch();
 
@@ -82,6 +104,7 @@ public class CongressionalActivity extends Activity {
         });
 
     }
+
 
     void findViews() {
         name1 = (TextView) findViewById(R.id.name1);
@@ -125,6 +148,7 @@ public class CongressionalActivity extends Activity {
     }
 
     void updateRepsInfo() {
+
         Representative first = new Representative("Doge", 0, R.drawable.dogeprofilepic);
         Representative second = new Representative("Nyan Cat", 1, R.drawable.nyan_cat);
         Representative third = new Representative("Trump", 2, R.drawable.trump);
@@ -167,4 +191,130 @@ public class CongressionalActivity extends Activity {
         Toast.makeText(getApplicationContext(), "Random Location! (" + x +", " + y+")", Toast.LENGTH_SHORT).show();
     }
 
+    void fetchRepData(int zip) {
+        //search zip
+        String searchZipURL = sunlightURL1 + "/legislators/locate?zip=" + zip +sunlightURL2;
+        checkConnection(searchZipURL);
+    }
+
+
+    public void checkConnection(String stringUrl) {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("No network connection available");
+        }
+    }
+
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+//         onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            parseRepProfiles(this, result);
+            // find and set bills for all representatives.
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 5000;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("HTTP CONNECTION", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    void decodeImage() { //todo
+        InputStream is = null;
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    public String getFromJSON(String in, String object, String child) {
+        try {
+            JSONObject reader = new JSONObject(in);
+            JSONObject sys = reader.getJSONObject(object);
+            return sys.getString(child);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void parseRepProfiles(DownloadWebpageTask webtask, String in) {
+        Representative firstRep = new Representative();
+        Representative secondRep = new Representative();
+        Representative thirdRep = new Representative();
+
+        reps.add(0, firstRep);
+        reps.add(1, secondRep);
+        reps.add(2, thirdRep);
+        int r = 0;
+        try {
+            JSONObject rootObject = new JSONObject(in);
+            JSONArray jsonReps = rootObject.optJSONArray("results");
+
+            for (Representative rep : reps) {
+                rep.setPersonalInfo(jsonReps.getJSONObject(r));
+                String searchZipURL = sunlightURL1 + "/bills?sponsor_id=" + rep.bioID + sunlightURL2;
+                rep.setBills(webtask.doInBackground(searchZipURL));
+                rep.setCommittees(this);
+                r++;
+            }
+        } catch (JSONException e) { Log.d("JSON error", "wooops"); }
+    }
 }
--- mobile/src/main/java/com/cs160/joleary/represent/Representative.java
+++ mobile/src/main/java/com/cs160/joleary/represent/Representative.java
@@ -2,6 +2,9 @@ package com.cs160.joleary.represent;
 
 import android.graphics.drawable.Drawable;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by TK on 3/3/16.
  */
@@ -11,8 +14,15 @@ public class Representative {
     public String URL = "defaultURL.com";
     public int imgID;
     public String partyName;
-    public int partyID;
+    public int partyID; //D = 1, R = 2, I = 3
     public int partyImg;
+    private String committees = "";
+    private String bills = "";
+    public String termEndDate = "";
+    public String twitter_id;
+    public String bioID;
+
+    public Representative() {}
 
     public Representative(String _name, String _email, String _URL, int _partyID, int _img) {
         name = _name;
@@ -31,7 +41,7 @@ public class Representative {
     }
 
     void letsparty() {
-        switch (partyID) {
+        switch (partyID) { //todo change "dem" -> "D" etc.
             case 0: partyImg = R.drawable.democrat_logo;
                 partyName = "dem";
                 break;
@@ -42,5 +52,38 @@ public class Representative {
                 partyName = "ind";
                 break;
         }
+    }
+
+    void setPersonalInfo(JSONObject res) {
+        try {
+            bioID = res.getString("bioguide_id");
+            name = res.getString("first_name") + " " + res.getString("last_name"); //what if no first or last name?
+            email = res.getString("oc_email");
+            partyName = res.getString("party");
+            twitter_id = res.getString("twitter_id");
+            termEndDate = res.getString("term_end");
+            URL = res.getString("website");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void setBills(String inJSON) {
+        try {
+            JSONObject reader = new JSONObject(inJSON);
+            JSONObject page = reader.getJSONObject("page");
+            int count = Integer.parseInt(page.getString("count"));
+            for (int i = 0; i<count; i++) {
+                JSONObject res = reader.getJSONObject("results");
+                bills = res.getString(count);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void setCommittees(String inJSON) {
+
     }
 }
